13fbf0a68bc599f94a89e14d827f9fe0b1529756
Jonathan Plata <jonathan_plata@ymail.com>
2021-05-25 16:39:42 -0700

    Javadoc comments for transferFunds

--- Common/src/main/java/edu/sdccd/cisc191/f/TransferFundsRequest.java
+++ Common/src/main/java/edu/sdccd/cisc191/f/TransferFundsRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
+ * Class that represents the Transfer Funds Requests. To transfer money between accounts.
  *
  * @author Oriel Sanchez
  * @author Eric Kim
@@ -17,16 +18,37 @@ public class TransferFundsRequest {
     @JsonIgnore
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Creates the transferFundsRequest object into json format to send within socket.
+     *
+     * @param  transferFundsRequest transferFundsRequest
+     * @return transferFundsRequest as a string
+     */
     public static String toJSON(TransferFundsRequest transferFundsRequest) throws Exception {
         return objectMapper.writeValueAsString(transferFundsRequest);
     }
 
+    /**
+     * Creates the transferFundsRequest object into json format to send within socket.
+     *
+     * @param  input          reads an transferFundsRequest formatted as a string
+     * @return converts the input string into an transferFundsRequest
+     */
     public static TransferFundsRequest fromJSON(String input) throws Exception {
         return objectMapper.readValue(input, TransferFundsRequest.class);
     }
 
     protected TransferFundsRequest() {}
 
+    /**
+     * Creates the transferFundsRequest object that accepts 4 parameters.
+     * This constructor is used to initialize the transferFundsRequest object in order to begin the transfer.
+     *
+     * @param requestType            one of four account request types: Account, Deposit, Withdraw, Transfer
+     * @param senderCardNumber       cardNumber that is sending the money
+     * @param transferAmount         amount to transfer
+     * @param recipientCardNumber    cardNumber receiving the money
+     */
     public TransferFundsRequest(RequestType requestType, long senderCardNumber, double transferAmount, long recipientCardNumber) {
         this.requestType = requestType;
         this.senderCardNumber = senderCardNumber;
@@ -34,34 +56,66 @@ public class TransferFundsRequest {
         this.recipientCardNumber = recipientCardNumber;
     }
 
+    /**
+     * Gets the requestType
+     *
+     * @return The requestType as an object of requestType
+     */
     public RequestType getRequestType() {
         return requestType;
     }
 
+    /**
+     * Sets the requestType
+     */
     public void setRequestType(RequestType requestType) {
         this.requestType = requestType;
     }
 
+    /**
+     * Gets the sender card number from the transferFundsRequest object
+     *
+     * @return the carbNumber as a long variable
+     */
     public long getSenderCardNumber() {
         return senderCardNumber;
     }
 
+    /**
+     * Sets the sender card number for transferFundsRequest object
+     */
     public void setSenderCardNumber(long senderCardNumber) {
         this.senderCardNumber = senderCardNumber;
     }
 
+    /**
+     * Gets the transfer amount from transferFundsRequest object
+     *
+     * @return the amount as a double
+     */
     public double getTransferAmount() {
         return transferAmount;
     }
 
+    /**
+     * Sets the transfer amount to the transferFundsRequest object
+     */
     public void setTransferAmount(double transferAmount) {
         this.transferAmount = transferAmount;
     }
 
+    /**
+     * Gets the receiving card number from transferFundsRequest object
+     *
+     * @return the cardNumber as a long
+     */
     public long getRecipientCardNumber() {
         return recipientCardNumber;
     }
 
+    /**
+     * Sets the receiving card number for transferFundsRequest object
+     */
     public void setRecipientCardNumber(long recipientCardNumber) {
         this.recipientCardNumber = recipientCardNumber;
     }
--- Common/src/main/java/edu/sdccd/cisc191/f/TransferFundsResponse.java
+++ Common/src/main/java/edu/sdccd/cisc191/f/TransferFundsResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- *
+ * Class that represents the Transfer Funds Responses. To transfer money between accounts.
  *
  * @author Oriel Sanchez
  * @author Eric Kim
@@ -15,33 +15,68 @@ public class TransferFundsResponse {
 
     @JsonIgnore
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Creates the transferFundsResponse object into json format to send within socket.
+     *
+     * @param  transferFundsResponse transferFundsResponse
+     * @return transferFundsResponse as a string
+     */
     public static String toJSON(TransferFundsResponse transferFundsResponse) throws Exception {
         return objectMapper.writeValueAsString(transferFundsResponse);
     }
 
+    /**
+     * Creates the transferFundsResponse object into json format to send within socket.
+     *
+     * @param  input  reads an transferFundsResponse formatted as a string
+     * @return converts the input string into an transferFundsResponse
+     */
     public static TransferFundsResponse fromJSON(String input) throws Exception {
         return objectMapper.readValue(input, TransferFundsResponse.class);
     }
 
     protected TransferFundsResponse() {}
 
+    /**
+     * Creates the transferFundsResponse object that accepts 2 parameters.
+     *
+     * @param transferSuccessful  boolean if the transfer was successful
+     * @param balance             new balance of the receiving account
+     */
     public TransferFundsResponse(boolean transferSuccessful, double balance) {
         this.transferSuccessful = transferSuccessful;
         this.balance = balance;
     }
 
+    /**
+     * Checks the status of the transfer
+     *
+     * @return if the transfer was successful
+     */
     public boolean isTransferSuccessful() {
         return transferSuccessful;
     }
 
+    /**
+     * Sets the status of the transfer
+     */
     public void setTransferSuccessful(boolean transferSuccessful) {
         this.transferSuccessful = transferSuccessful;
     }
 
+    /**
+     * Gets the new balance of the account receiving the money
+     *
+     * @return the new balance as a double
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Sets the new balance after the money is transferred
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
